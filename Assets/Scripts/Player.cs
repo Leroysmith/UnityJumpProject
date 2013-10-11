85dffdb099cc1449ccfcd96dab99dc310168eafa
Leroysmith <ldsmith@live.nl>
2013-10-11 16:34:30 +0200

    Jump updated again

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,31 +13,37 @@ public class Player : MonoBehaviour {
 	// Update is called once per frame
 		void FixedUpdate () {
 		Vector3 snelheid = new Vector3();
+		float jumpSpeed = 40f;
+		float runSpeed = 20f;
+		
 		
 		snelheid.y = rigidbody.velocity.y;
 		
 		if (Input.GetAxis("Horizontal") < 0)
 		{
-			snelheid.x = -10;
+			snelheid.x = -runSpeed;
+			rigidbody.velocity = snelheid;
 		}
 		else if (Input.GetAxis("Horizontal") > 0)
 		{
-			snelheid.x = 10;
+			snelheid.x = runSpeed;
+			rigidbody.velocity = snelheid;
 		}
 		else
 		{
 			snelheid.x = 0;
 		}
 		
-		if (Input.GetAxis("Jump") > 0 && isOnPlatform)
+		if (Input.GetAxis("Jump") > 0)
 		{
-			rigidbody.AddForce(new Vector3(0, 400, 0));
-			isOnPlatform = false;
+			transform.position += transform.up * jumpSpeed * Time.deltaTime;
 		}
 		
-		rigidbody.velocity = snelheid;
+
 		
-		if(transform.position.y < -8.5)
+	
+		
+		if(transform.position.y < -800)
 		{
 			Application.LoadLevel (0);
 		}
